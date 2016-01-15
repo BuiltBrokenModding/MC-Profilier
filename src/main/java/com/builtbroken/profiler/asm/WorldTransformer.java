5bbcfe59a728a644a93e9fc14e03e646eeec6a32
Robert Seifert <rseifert.phone@gmail.com>
2016-01-14 23:24:10 -0500

    More asm work inject calls into set block methods

--- src/main/java/com/builtbroken/profiler/asm/WorldTransformer.java
+++ src/main/java/com/builtbroken/profiler/asm/WorldTransformer.java
@@ -1,16 +1,17 @@
 package com.builtbroken.profiler.asm;
 
+import net.minecraft.block.Block;
 import net.minecraft.launchwrapper.IClassTransformer;
+import net.minecraft.world.World;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
 
 import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -20,6 +21,7 @@ public class WorldTransformer implements IClassTransformer
 {
     private static final String CLASS_KEY_WORLD = "net.minecraft.world.World";
     private static final String CLASS_KEY_BLOCK = "net.minecraft.block.Block";
+    private static final String BLOCK_HOOK_CLASS = "com/builtbroken/profiler/hooks/BlockHooks";
 
     private static final HashMap<String, SimpleEntry<String, String>> entryMap = new HashMap<String, SimpleEntry<String, String>>();
 
@@ -35,30 +37,80 @@ public class WorldTransformer implements IClassTransformer
     @Override
     public byte[] transform(String name, String transformerName, byte[] bytes)
     {
-        System.out.println("transform(" + name + "," + transformerName + ", bytes)");
         String changedName = name.replace('.', '/');
-
         if (changedName.equals(getName(CLASS_KEY_WORLD)))
         {
-            System.out.println("\tTransformer editing world class");
             ClassNode cn = startInjection(bytes);
-            MethodNode setBlockMethod = getMethod(cn, "setBlock", "(IIIL" + getName(CLASS_KEY_BLOCK) + ";II)Z");
-
-            if (setBlockMethod != null)
-            {
-
-                final InsnList nodeAdd = new InsnList();
-                final InsnList nodeAdd2 = new InsnList();
-
-                nodeAdd.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/builtbroken/profiler/hooks/BlockHooks", "onBlockChangeStart", "", false));
-                nodeAdd2.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/builtbroken/profiler/hooks/BlockHooks", "onBlockChangeEnd", "", false));
-
-                setBlockMethod.instructions.insertBefore(setBlockMethod.instructions.get(0), nodeAdd);
-                setBlockMethod.instructions.insert(setBlockMethod.instructions.get(setBlockMethod.instructions.size() - 1), nodeAdd2);
-            }
+            injectSetBlock(cn);
+            injectSetBlockWithMeta(cn);
             return finishInjection(cn);
         }
         return bytes;
+    }
+
+    /** {@link World#setBlock(int, int, int, Block, int, int)} */
+    private void injectSetBlock(ClassNode cn)
+    {
+        MethodNode setBlockMethod = getMethod(cn, "setBlock", "(IIIL" + getName(CLASS_KEY_BLOCK) + ";II)Z");
+
+        if (setBlockMethod != null)
+        {
+            //Create method call
+            final InsnList nodeAdd = new InsnList();
+
+            nodeAdd.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            nodeAdd.add(new VarInsnNode(Opcodes.ILOAD, 1));
+            nodeAdd.add(new VarInsnNode(Opcodes.ILOAD, 2));
+            nodeAdd.add(new VarInsnNode(Opcodes.ILOAD, 3));
+            nodeAdd.add(new MethodInsnNode(Opcodes.INVOKESTATIC, BLOCK_HOOK_CLASS, "onBlockChange", "(L" + getName(CLASS_KEY_WORLD) + ";III)V", false));
+
+            //Inject method call at top of method
+            setBlockMethod.instructions.insertBefore(setBlockMethod.instructions.get(0), nodeAdd);
+
+            //Locate all return points from the method
+            List<AbstractInsnNode> returnNodes = new ArrayList();
+            for (int i = 0; i < setBlockMethod.instructions.size(); i++)
+            {
+                AbstractInsnNode ain = setBlockMethod.instructions.get(i);
+                if (ain.getOpcode() == Opcodes.IRETURN)
+                {
+                    returnNodes.add(ain);
+                }
+            }
+
+            //Inject calls in front of return points
+            for (AbstractInsnNode node : returnNodes)
+            {
+                //Create method call
+                final InsnList nodeAdd2 = new InsnList();
+                nodeAdd2.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                nodeAdd2.add(new VarInsnNode(Opcodes.ILOAD, 1));
+                nodeAdd2.add(new VarInsnNode(Opcodes.ILOAD, 2));
+                nodeAdd2.add(new VarInsnNode(Opcodes.ILOAD, 3));
+                nodeAdd2.add(new MethodInsnNode(Opcodes.INVOKESTATIC, BLOCK_HOOK_CLASS, "onPostBlockChange", "(L" + getName(CLASS_KEY_WORLD) + ";III)V", false));
+                //Inject method call before return node
+                setBlockMethod.instructions.insertBefore(node, nodeAdd2);
+            }
+        }
+    }
+
+    /** {@link World#setBlockMetadataWithNotify(int, int, int, int, int)} */
+    private void injectSetBlockWithMeta(ClassNode cn)
+    {
+        MethodNode setBlockMetaMethod = getMethod(cn, "setBlockMetadataWithNotify", "(IIIII)Z");
+
+        if (setBlockMetaMethod != null)
+        {
+            final InsnList nodeAdd = new InsnList();
+
+            nodeAdd.add(new VarInsnNode(Opcodes.ALOAD, 0)); //this
+            nodeAdd.add(new VarInsnNode(Opcodes.ILOAD, 1)); //x
+            nodeAdd.add(new VarInsnNode(Opcodes.ILOAD, 2)); //y
+            nodeAdd.add(new VarInsnNode(Opcodes.ILOAD, 3)); //z
+            nodeAdd.add(new MethodInsnNode(Opcodes.INVOKESTATIC, BLOCK_HOOK_CLASS, "onBlockMetaChange", "(L" + getName(CLASS_KEY_WORLD) + ";III)V", false));
+
+            setBlockMetaMethod.instructions.insertBefore(setBlockMetaMethod.instructions.get(0), nodeAdd);
+        }
     }
 
     private ClassNode startInjection(byte[] bytes)
--- src/main/java/com/builtbroken/profiler/hooks/BlockHooks.java
+++ src/main/java/com/builtbroken/profiler/hooks/BlockHooks.java
@@ -1,18 +1,50 @@
 package com.builtbroken.profiler.hooks;
 
+import com.builtbroken.jlib.lang.StringHelpers;
+import net.minecraft.world.World;
+
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 1/14/2016.
  */
 public class BlockHooks
 {
-    public void onBlockChangeStart()
+    public static void onBlockChange(World world, int x, int y, int z)
     {
-        System.out.println("onBlockChangeStart");
+        System.out.println("onBlockChange(" + world + ", " + x + ", " + y + ", " + z + ")");
+        long time = System.nanoTime();
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        time = System.nanoTime() - time;
+        StackTraceElement element = trace[0];
+        System.out.println("\t" + element.getMethodName());
+        element = trace[1];
+        System.out.println("\t" + element.getMethodName());
+        element = trace[2];
+        System.out.println("\t" + element.getMethodName());
+        element = trace[3];
+        System.out.println("\t" + element.getMethodName());
+        element = trace[4];
+        System.out.println("\t" + element.getMethodName());
+        System.out.println("\t" + StringHelpers.formatNanoTime(time));
     }
 
-    public void onBlockChangeEnd()
+    public static void onPostBlockChange(World world, int x, int y, int z)
     {
-        System.out.println("onBlockChangeEnd");
+        System.out.println("onPostBlockChange(" + world + ", " + x + ", " + y + ", " + z + ")");
+    }
+
+    public static void onBlockMetaChange(World world, int x, int y, int z)
+    {
+        //System.out.println("onBlockMetaChange(" + world + ", " + x + ", " + y + ", " + z + ")");
+    }
+
+    public static void onPostBlockMetaChange(World world, int x, int y, int z)
+    {
+        //System.out.println("onPostBlockMetaChange(" + world + ", " + x + ", " + y + ", " + z + ")");
+    }
+
+    public void postOnBlockChange()
+    {
+
     }
 }
